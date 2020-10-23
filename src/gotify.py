8e71192d0fde758afa5211e853d6d0a0dd5f9934
Scott Wallace <scott@wallace.sh>
2020-10-23 14:11:32 +0100

    Use logging to handle console messaging

--- src/gotify.py
+++ src/gotify.py
@@ -3,13 +3,13 @@ Module to handle communication with the Gotify server
 """
 import http.client
 import json
+import logging
 
 
 class Gotify:
     """
     Class to handle Gotify communications
     """
-    verbose = False
 
     def __init__(self, server, port, app_key, client_key=None):
         self.api = http.client.HTTPConnection(server, port)
@@ -30,14 +30,13 @@ class Gotify:
         else:
             headers['X-Gotify-Key'] = self.app_key
 
-        if self.verbose:
-            print(f'DEBUG: Sending to Gotify: {body}')
+        logging.debug('Sending to Gotify:\n%s', body)
 
         try:
             self.api.request(method, url, body=body, headers=headers)
             response = self.api.getresponse()
         except ConnectionRefusedError as error:
-            print(f'ERROR: {error}')
+            logging.error(error)
             return {
                 'status': error.errno,
                 'reason': error.strerror
@@ -53,11 +52,10 @@ class Gotify:
             try:
                 resp_obj['json'] = json.loads(rawbody.decode())
             except json.decoder.JSONDecodeError as error:
-                print(f'ERROR: {error}')
+                logging.error(error)
 
-        if self.verbose:
-            print(f'DEBUG: Returned from Gotify: {json.dumps(resp_obj, indent=2)}')
-            print('DEBUG: Status: {status}, Reason: {reason}'.format(**resp_obj))
+        logging.debug('Returned from Gotify:\n%s', json.dumps(resp_obj, indent=2))
+        logging.debug('Status: %s, Reason: %s', resp_obj['status'], resp_obj['reason'])
 
         return resp_obj
 
@@ -65,8 +63,7 @@ class Gotify:
         """
         Method to delete a message from the Gotify server
         """
-        if self.verbose:
-            print(f'DEBUG: Deleting message ID {msg_id}')
+        logging.debug('Deleting message ID: %s', msg_id)
         return self._call('DELETE', f'/message/{msg_id}')
 
     def find_byfingerprint(self, message):
@@ -76,8 +73,7 @@ class Gotify:
         try:
             new_fingerprint = message['fingerprint']
         except KeyError:
-            if self.verbose:
-                print('DEBUG: No fingerprint found in new message')
+            logging.debug('No fingerprint found in new message')
             return None
 
         for old_message in self.messages():
@@ -86,31 +82,25 @@ class Gotify:
                 if old_fingerprint == new_fingerprint:
                     return old_message['id']
             except KeyError:
-                if self.verbose:
-                    print(
-                        f'DEBUG: No fingerprint found in message {old_message["id"]}'
-                    )
+                logging.debug('No fingerprint found in message ID: %s', old_message['id'])
                 continue
 
-        if self.verbose:
-            print('DEBUG: No fingerprint matched.')
+        logging.debug('No fingerprint matched.')
         return None
 
     def messages(self):
         """
         Method to return a list of messages from the Gotify server
         """
-        if not self.client_key and self.verbose:
-            print('DEBUG: No client key is configured.  No messages could be retrieved.')
+        if not self.client_key:
+            logging.debug('No client key is configured.  No messages could be retrieved.')
             return []
-        if self.verbose:
-            print('DEBUG: Fetching existing messages from Gotify')
+        logging.debug('Fetching existing messages from Gotify')
         return self._call('GET', '/message')['json'].get('messages', [])
 
     def send_alert(self, payload):
         """
         Method to send a message payload to a Gotify server
         """
-        if self.verbose:
-            print('DEBUG: Sending message to Gotify')
+        logging.debug('Sending message to Gotify')
         return self._call('POST', '/message', body=json.dumps(payload, indent=2))
