12b39b1c08bd4bc40faac8caacf7f25bb33f1ff5
Scott Wallace <scott@wallace.sh>
2020-10-23 10:06:51 +0100

    Add the ability to delete original alert when matching resolved message arrives

--- test.sh
+++ test.sh
@@ -2,56 +2,86 @@
 
 SERVER=${1:-'localhost:8080'}
 
-name=testAlert-$RANDOM
+NAME=testAlert-$RANDOM
+FINGERPRINT=$(date +%s | md5sum | cut -f1 -d' ')
 URL="http://${SERVER}/alert"
-bold=$(tput bold)
-normal=$(tput sgr0)
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
 
 call_alertmanager() {
-    curl -v "${URL}" --header 'Content-type: application/json' --data @<(cat <<EOF
+    VALUE=${1}
+    curl -v "${URL}" --header 'Expect:' --header 'Content-type: application/json' --data @<(cat <<EOF
 {
-  "version": "4",
-  "groupKey": "testGroup",
-  "truncatedAlerts": 0,
-  "status": "${STATUS}",
   "receiver": "alertify",
-  "commonLabels": {
-    "alertname": "${name}",
-    "service": "testService",
-    "severity":"warning",
-    "instance": "server.example.net",
-    "namespace": "testNamespace",
-    "label_costcentre": "testCostCentre"
-  },
-  "commonAnnotations": {
-    "summary": "Testing latency is high!",
-    "description": "Testing latency is at ${1}"
-  },
+  "status": "${STATUS}",
   "alerts": [
     {
       "status": "${STATUS}",
-      "generatorURL": "http://alertmanager.example.net/$name",
+      "labels": {
+        "alertname": "${NAME}",
+        "id": "01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b",
+        "instance": "localhost:1234",
+        "job": "test_job",
+        "name": "testserver",
+        "priority": "1",
+        "severity": "low",
+        "value": "${VALUE}"
+      },
+      "annotations": {
+        "description": "testserver: unhealthy",
+        "summary": "Server unhealthy"
+      },
       "startsAt": "${START}",
-      "endsAt": "${END}"
+      "endsAt": "${END}",
+      "generatorURL": "http://example.com/some/url",
+      "fingerprint": "${FINGERPRINT}"
     }
-  ]
+  ],
+  "groupLabels": {
+    "alertname": "${NAME}",
+    "id": "01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b",
+    "instance": "localhost:1234",
+    "job": "test_job",
+    "name": "testserver",
+    "priority": "1",
+    "severity": "low",
+    "value": "${VALUE}"
+  },
+  "commonLabels": {
+    "alertname": "${NAME}",
+    "id": "01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b",
+    "instance": "localhost:1234",
+    "job": "test_job",
+    "name": "testserver",
+    "priority": "1",
+    "severity": "low",
+    "value": "${VALUE}"
+  },
+  "commonAnnotations": {
+    "description": "testserver: unhealthy",
+    "summary": "Server unhealthy"
+  },
+  "externalURL": "http://1ff297bc31a0:9093",
+  "version": "4",
+  "groupKey": "{}:{alertname=\"${NAME}\", id=\"01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b\", instance=\"localhost:1234\", job=\"test_job\", name=\"testserver\", priority=\"1\", severity=\"low\", value=\"${VALUE}\"}",
+  "truncatedAlerts": 0
 }
 EOF
 )
 }
 
-echo "${bold}Firing alert ${name} ${normal}"
+echo "${BOLD}Firing alert ${NAME} ${NORMAL}"
 STATUS='firing'
 START=$(date --rfc-3339=seconds | sed 's/ /T/')
 END="0001-01-01T00:00:00Z"
 call_alertmanager 42
 echo -e "\n"
 
-echo "${bold}Press enter to resolve alert ${name} ${normal}"
+echo "${BOLD}Press enter to resolve alert ${NAME} ${NORMAL}"
 read -r
 
 
-echo "${bold}Sending resolved ${normal}"
+echo "${BOLD}Sending resolved ${NORMAL}"
 STATUS='resolved'
 END=$(date --rfc-3339=seconds | sed 's/ /T/')
 call_alertmanager 0
